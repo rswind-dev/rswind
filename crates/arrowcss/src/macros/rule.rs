ed3d8d418aad2c0cc2bc1edc0590c5460ccf227d
liangzhengtai <liangzhengtai@didiglobal.com>
2023-12-01 12:04:59 +0800

    chore: move macros

---
+++ crates/arrowcss/src/macros/rule.rs
@@ -0,0 +1,18 @@
+#[macro_export]
+macro_rules! static_rules {
+  (
+    $($key:literal => {
+      $($name:literal: $value:literal;)+
+    })+
+  ) => {
+    vec![
+      $(
+        ($key, CSSDecls::multi([
+          $(
+            ($name, $value),
+          )+
+        ])),
+      )+
+    ]
+  };
+}
