dd7aed3b2db23771fc19751161bb0e6bb1dd782b
lzt1008 <lzt1008@live.com>
2024-03-22 01:57:49 +0800

    feat: map macro

--- crates/arrowcss/src/macros/mod.rs
+++ crates/arrowcss/src/macros/mod.rs
@@ -1,1 +1,14 @@
 pub mod rule;
+
+#[macro_export]
+macro_rules! map {
+    ($($key:expr => $value:expr),*) => {
+        {
+            let mut m = std::collections::HashMap::new();
+            $(
+                m.insert($key.to_string(), $value.to_string());
+            )*
+            m
+        }
+    };
+}
