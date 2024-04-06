f295911ca9c43c7daeab4b2354c83d9d6af93c63
lzt1008 <lzt1008@live.com>
2024-04-06 14:51:38 +0800

    feat: impl ToCss to all IntoIterator

--- crates/arrowcss/src/css/container.rs
+++ crates/arrowcss/src/css/container.rs
@@ -10,67 +10,16 @@ use super::ToCss;
 // #[derive(Debug, Default)]
 pub type NodeList<'a> = Vec<AstNode<'a>>;
 
-// impl<'a> Clone for NodeList<'a> {
-//     fn clone(&self) -> Self {
-//         Self {
-//             nodes: self.nodes.clone(),
-//         }
-//     }
-// }
-
-// impl<'a> FromIterator<AstNode<'a>> for NodeList<'a> {
-//     fn from_iter<T: IntoIterator<Item = AstNode<'a>>>(iter: T) -> Self {
-//         Self {
-//             nodes: iter.into_iter().collect(),
-//         }
-//     }
-// }
-
-// impl<'a> FromIterator<NodeList<'a>> for NodeList<'a> {
-//     fn from_iter<T: IntoIterator<Item = NodeList<'a>>>(iter: T) -> Self {
-//         Self {
-//             nodes: iter.into_iter().flat_map(|c| c.nodes).collect(),
-//         }
-//     }
-// }
-
-// impl From<T> for some base type for CssRuleList
-// impl<'a> From<DeclList<'a>> for NodeList<'a> {
-//     fn from(decls: DeclList<'a>) -> Self {
-//         decls.0.into_iter().map(AstNode::Decl).collect()
-//     }
-// }
-
-// impl<'a> From<Decl<'a>> for NodeList<'a> {
-//     fn from(decl: Decl<'a>) -> Self {
-//         AstNode::Decl(decl).into()
-//     }
-// }
-
-// impl<'a, const N: usize> From<[Decl<'a>; N]> for NodeList<'a> {
-//     fn from(decls: [Decl<'a>; N]) -> Self {
-//         decls.iter().map(|d| AstNode::Decl(d.clone())).collect()
-//     }
-// }
-
-// impl<'a> From<AstNode<'a>> for NodeList<'a> {
-//     fn from(rule: AstNode<'a>) -> Self {
-//         Self { nodes: vec![rule] }
-//     }
-// }
-
-// impl<'a> From<Rule<'a>> for NodeList<'a> {
-//     fn from(rule: Rule<'a>) -> Self {
-//         AstNode::Rule(rule).into()
-//     }
-// }
-
-impl<'a> ToCss for NodeList<'a> {
-    fn to_css<W>(&self, writer: &mut Writer<W>) -> Result<(), Error>
+impl<'a, 'b, T> ToCss for T
+where
+    'a: 'b,
+    T: IntoIterator<Item = &'b AstNode<'a>>,
+{
+    fn to_css<W>(self, writer: &mut Writer<W>) -> Result<(), Error>
     where
         W: Write,
     {
-        let mut iter = self.iter();
+        let mut iter = self.into_iter();
         if let Some(first) = iter.next() {
             first.to_css(writer)?;
             for node in iter {
