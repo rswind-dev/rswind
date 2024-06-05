784b1088ea0c9253f7d8a4597249af4d024cf4be
lzt1008 <lzt1008@live.com>
2024-06-05 17:42:58 +0800

    feat: impl `FromStr` for `OrderingKey`

--- crates/rswind/src/ordering.rs
+++ crates/rswind/src/ordering.rs
@@ -1,4 +1,4 @@
-use std::hash::Hash;
+use std::{hash::Hash, str::FromStr};
 
 use serde::Deserialize;
 
@@ -54,4 +54,70 @@ pub enum OrderingKey {
 
     Grouped,
     Property,
+}
+
+impl FromStr for OrderingKey {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s {
+            "translate" => Self::Translate,
+            "translate-x" | "translate-y" => Self::TranslateAxis,
+            "scale" => Self::Scale,
+            "scale-x" | "scale-y" => Self::ScaleAxis,
+            "rotate" => Self::Rotate,
+            "rotate-x" | "rotate-y" => Self::RotateAxis,
+            "skew" => Self::Skew,
+            "skew-x" | "skew-y" => Self::SkewAxis,
+            "transform" => Self::Transform,
+
+            "margin" => Self::Margin,
+            "margin-x" | "margin-y" => Self::MarginAxis,
+            "margin-top" | "margin-right" | "margin-bottom" | "margin-left" => Self::MarginSide,
+
+            "padding" => Self::Padding,
+            "padding-x" | "padding-y" => Self::PaddingAxis,
+            "padding-top" | "padding-right" | "padding-bottom" | "padding-left" => {
+                Self::PaddingSide
+            }
+
+            "space-x" | "space-y" => Self::SpaceAxis,
+
+            "rounded" => Self::Rounded,
+            "rounded-top-left"
+            | "rounded-top-right"
+            | "rounded-bottom-right"
+            | "rounded-bottom-left" => Self::RoundedCorner,
+            "rounded-t" | "rounded-r" | "rounded-b" | "rounded-l" => Self::RoundedSide,
+
+            "inset" => Self::Inset,
+            "inset-x" | "inset-y" => Self::InsetAxis,
+            "inset-top" | "inset-right" | "inset-bottom" | "inset-left" => Self::InsetSide,
+            "top" | "right" | "bottom" | "left" => Self::PositionSide,
+
+            "border-spacing" => Self::BorderSpacing,
+            "border-spacing-x" | "border-spacing-y" => Self::BorderSpacingAxis,
+
+            "border-color" => Self::BorderColor,
+            "border-color-x" | "border-color-y" => Self::BorderColorAxis,
+            "border-color-top"
+            | "border-color-right"
+            | "border-color-bottom"
+            | "border-color-left" => Self::BorderColorSide,
+
+            "border-width" => Self::BorderWidth,
+            "border-right-width"
+            | "border-left-width"
+            | "border-top-width"
+            | "border-bottom-width" => Self::BorderWidthAxis,
+            "border-top-right-width"
+            | "border-top-left-width"
+            | "border-bottom-right-width"
+            | "border-bottom-left-width" => Self::BorderWidthSide,
+
+            "w" | "h" => Self::SizeAxis,
+
+            _ => return Err(()),
+        })
+    }
 }
