26350b6bb31b851af2bbdd480cfd744c9dab07a7
lzt1008 <lzt1008@live.com>
2024-06-07 19:29:29 +0800

    chore: auto generated named exports

--- packages/rswind/tsup.config.ts
+++ packages/rswind/tsup.config.ts
@@ -1,10 +1,41 @@
+import { execSync } from 'node:child_process'
+import { readFileSync } from 'node:fs'
+import { resolve } from 'node:path'
+
+import type { Options } from 'tsup'
 import { defineConfig } from 'tsup'
+
+const generateNamedExportPlugin: Required<Options>['esbuildPlugins'][0] = {
+  name: 'rswind:generate-named-export',
+  setup(build) {
+    if (build.initialOptions.entryPoints?.length !== 1) {
+      throw new Error('entryPoints must have exactly one item')
+    }
+
+    const entry = resolve(__dirname, build.initialOptions.entryPoints[0])
+    const entryRe = new RegExp(`^${entry}$`)
+
+    build.onLoad({ filter: entryRe }, async (_args) => {
+      const output = execSync('../../scripts/generate-binding.ts', {
+        stdio: 'pipe',
+      })
+
+      return {
+        contents: `${readFileSync(entry)}\n${output.toString()}`,
+        loader: 'ts',
+      }
+    })
+  },
+}
 
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm', 'cjs'],
   target: 'node16',
   inject: ['src/esm-shims.ts'],
+  esbuildPlugins: [
+    generateNamedExportPlugin,
+  ],
   clean: true,
   dts: true,
   shims: true,
---
+++ scripts/generate-binding.ts
@@ -0,0 +1,162 @@
+#!/usr/bin/env -S deno run -A
+
+import { sortBy } from '@std/collections'
+import { bold, red } from '@std/fmt/colors'
+
+enum TypeDefKind {
+  Const = 'const',
+  Enum = 'enum',
+  StringEnum = 'string_enum',
+  Interface = 'interface',
+  Fn = 'fn',
+  Struct = 'struct',
+  Impl = 'impl',
+}
+
+interface TypeDefLine {
+  kind: TypeDefKind
+  name: string
+  original_name?: string
+  def: string
+  js_doc?: string
+  js_mod?: string
+}
+
+const TOP_LEVEL_NAMESPACE = '__TOP_LEVEL_MODULE__'
+
+async function readIntermediateTypeFile(file: string) {
+  const content = await Deno.readTextFile(file)
+  const defs = content
+    .split('\n')
+    .filter(Boolean)
+    .map((line) => {
+      line = line.trim()
+      if (!line.startsWith('{')) {
+        // crateName:{ "def": "", ... }
+        const start = line.indexOf(':') + 1
+        line = line.slice(start)
+      }
+      return JSON.parse(line) as TypeDefLine
+    })
+
+  // move all `struct` def to the very top
+  // and order the rest alphabetically.
+  return defs.sort((a, b) => {
+    if (a.kind === TypeDefKind.Struct) {
+      if (b.kind === TypeDefKind.Struct) {
+        return a.name.localeCompare(b.name)
+      }
+      return -1
+    }
+    else if (b.kind === TypeDefKind.Struct) {
+      return 1
+    }
+    else {
+      return a.name.localeCompare(b.name)
+    }
+  })
+}
+
+function preprocessTypeDef(defs: TypeDefLine[]): Map<string, TypeDefLine[]> {
+  const namespaceGrouped = new Map<string, TypeDefLine[]>()
+  const classDefs = new Map<string, TypeDefLine>()
+
+  for (const def of defs) {
+    const namespace = def.js_mod ?? TOP_LEVEL_NAMESPACE
+    if (!namespaceGrouped.has(namespace)) {
+      namespaceGrouped.set(namespace, [])
+    }
+
+    const group = namespaceGrouped.get(namespace)!
+
+    if (def.kind === TypeDefKind.Struct) {
+      group.push(def)
+      classDefs.set(def.name, def)
+    }
+    else if (def.kind === TypeDefKind.Impl) {
+      // merge `impl` into class definition
+      const classDef = classDefs.get(def.name)
+      if (classDef) {
+        if (classDef.def) {
+          classDef.def += '\n'
+        }
+
+        classDef.def += def.def
+      }
+    }
+    else {
+      group.push(def)
+    }
+  }
+
+  return namespaceGrouped
+}
+
+export async function processTypeDef(
+  intermediateTypeFile: string,
+) {
+  const exports: string[] = []
+  const defs = await readIntermediateTypeFile(intermediateTypeFile)
+  const groupedDefs = preprocessTypeDef(defs)
+
+  sortBy(Array.from(groupedDefs), ([namespace]) => namespace).forEach(
+    ([namespace, defs]) => {
+      if (namespace === TOP_LEVEL_NAMESPACE) {
+        for (const def of defs) {
+          switch (def.kind) {
+            case TypeDefKind.Const:
+            case TypeDefKind.Enum:
+            case TypeDefKind.StringEnum:
+            case TypeDefKind.Fn:
+            case TypeDefKind.Struct: {
+              exports.push(def.name)
+              if (def.original_name && def.original_name !== def.name) {
+                exports.push(def.original_name)
+              }
+              break
+            }
+            default:
+              break
+          }
+        }
+      }
+      else {
+        exports.push(namespace)
+      }
+    },
+  )
+  return exports
+}
+
+if (import.meta.main) {
+  const tempFilePath = await Deno.makeTempFile()
+
+  const generateTypeCommand = new Deno.Command('cargo', {
+    args: [
+      'check',
+      '-p',
+      'binding_core_node',
+      '--color',
+      'always',
+    ],
+    stdout: 'inherit',
+    stderr: 'inherit',
+    env: {
+      TYPE_DEF_TMP_PATH: tempFilePath,
+    },
+  })
+
+  const generateTypeOutput = await generateTypeCommand.output()
+
+  if (!generateTypeOutput.success) {
+    Deno.stderr.write(generateTypeOutput.stderr)
+    console.error(red(bold('Something went wrong while generating types ↑')))
+    Deno.exit(generateTypeOutput.code)
+  }
+
+  const idents = await processTypeDef(tempFilePath)
+
+  const exports = idents.map(ident => `export const ${ident} = binding.${ident}`).join('\n')
+
+  await Deno.stdout.write(new TextEncoder().encode(exports))
+}
