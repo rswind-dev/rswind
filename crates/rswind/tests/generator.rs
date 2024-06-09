4d54d4cc20f139c8165b32ee735c9798b4663490
lzt1008 <lzt1008@live.com>
2024-06-09 15:01:54 +0800

    chore: fmt

--- crates/rswind/tests/generator.rs
+++ crates/rswind/tests/generator.rs
@@ -30,15 +30,9 @@ mod generator_tests {
             )
             .build()
             .unwrap();
-        assert_eq!(
-            generator.theme().get_value("colors", "primary").as_deref(),
-            Some("#3490dc")
-        );
+        assert_eq!(generator.theme().get_value("colors", "primary").as_deref(), Some("#3490dc"));
 
-        assert_eq!(
-            generator.theme().get_value("colors", "red-500").as_deref(),
-            None
-        );
+        assert_eq!(generator.theme().get_value("colors", "red-500").as_deref(), None);
     }
 
     #[test]
@@ -65,19 +59,10 @@ mod generator_tests {
             .build()
             .unwrap();
 
-        assert_eq!(
-            generator.theme().get_value("colors", "primary").as_deref(),
-            Some("#3490dc")
-        );
-        assert_eq!(
-            generator.theme().get_value("colors", "red-500").as_deref(),
-            Some("#ef4444")
-        );
+        assert_eq!(generator.theme().get_value("colors", "primary").as_deref(), Some("#3490dc"));
+        assert_eq!(generator.theme().get_value("colors", "red-500").as_deref(), Some("#ef4444"));
 
-        assert_eq!(
-            generator.theme().get_value("spacing", "1").as_deref(),
-            Some("0.25rem")
-        );
+        assert_eq!(generator.theme().get_value("spacing", "1").as_deref(), Some("0.25rem"));
         assert_eq!(generator.theme().get("spacing").unwrap().deref().len(), 1);
         assert!(generator.theme().get("keyframes").unwrap().deref().is_empty());
     }
