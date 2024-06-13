5fbd9ad955b90f869e47c74539edf1f17af77677
lzt1008 <lzt1008@live.com>
2024-06-14 02:15:28 +0800

    chore(wasm): new api

--- bindings/core_wasm/src/lib.rs
+++ bindings/core_wasm/src/lib.rs
@@ -1,6 +1,7 @@
-use std::sync::{Arc, RwLock};
-
-use rswind::{create_processor, processor::GeneratorProcessor};
+use rswind::{
+    generator::GeneratorBuilder, preset::preset_tailwind, processor::GeneratorProcessor,
+    GeneratorConfig,
+};
 use rswind_extractor::{Extractable, Extractor};
 use wasm_bindgen::prelude::*;
 
@@ -11,16 +12,36 @@ pub fn set_panic_hook() {
     console_error_panic_hook::set_once();
 }
 
-lazy_static::lazy_static! {
-    static ref APP: Arc<RwLock<GeneratorProcessor>> = Arc::new(RwLock::new(create_processor()));
+#[wasm_bindgen]
+pub struct Generator {
+    processor: GeneratorProcessor,
+}
+
+#[wasm_bindgen(js_name = createGenerator)]
+pub fn create_generator(config: Option<GeneratorConfig>) -> Result<Generator, JsError> {
+    Generator::new(config)
 }
 
 #[wasm_bindgen]
-pub fn generate(css: String, typ: String) -> String {
-    APP.write().unwrap().run_with(Extractor::new(&css, &*typ).extract()).css.as_str().to_string()
-}
+impl Generator {
+    #[wasm_bindgen(constructor)]
+    pub fn new(config: Option<GeneratorConfig>) -> Result<Generator, JsError> {
+        Ok(Generator {
+            processor: GeneratorBuilder::new()
+                .with_parallel(false)
+                .with_preset(preset_tailwind)
+                .with_config(config.unwrap_or_default())
+                .build_processor()?,
+        })
+    }
 
-#[wasm_bindgen(js_name = generateWith)]
-pub fn generate_with(candidates: Vec<String>) -> String {
-    APP.write().unwrap().run_with(candidates).css.as_str().to_string()
+    #[wasm_bindgen]
+    pub fn generate(&mut self, css: String, typ: String) -> String {
+        self.processor.run_with(Extractor::new(&css, &*typ).extract()).css.as_str().to_owned()
+    }
+
+    #[wasm_bindgen(js_name = generateWith)]
+    pub fn generate_with(&mut self, candidates: Vec<String>) -> String {
+        self.processor.run_with(candidates).css.as_str().to_owned()
+    }
 }
