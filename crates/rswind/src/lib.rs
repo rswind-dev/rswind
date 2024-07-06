a1bb2d0a25492181a8fd904073df233f2c959a0f
lzt1008 <lzt1008@live.com>
2024-07-06 10:57:35 +0800

    chore: rename folder

--- crates/rswind/src/lib.rs
+++ crates/rswind/src/lib.rs
@@ -1,35 +1,3 @@
-pub mod cache;
-pub mod common;
-pub mod config;
-pub mod context;
-pub mod generator;
-pub mod glob;
-pub mod io;
-pub mod ordering;
-pub mod parsing;
-pub mod process;
-pub mod processor;
-pub mod types;
+pub use rswind_core::*;
 
-pub use config::GeneratorConfig;
-pub use generator::Generator;
-
-pub use context::DesignSystem;
-
-pub mod css {
-    pub use rswind_css::*;
-    pub use rswind_css_macro::*;
-}
-
-pub mod theme {
-    pub use rswind_theme::*;
-}
-
-pub mod extract {
-    pub use rswind_extractor::*;
-}
-
-pub mod build {
-    pub use crate::config::StaticUtilityConfig;
-    pub use crate::parsing::UtilityInput;
-}
+pub mod preset;
--- crates/rswind_shell/src/lib.rs
+++
@@ -1,3 +0,0 @@
-pub use rswind_core::*;
-
-pub mod preset;
